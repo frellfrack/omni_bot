a30b51c4138539bedf084a6be6953781252681fa
frellfrack <dom.wormald@gmail.com>
2025-07-05 23:49:47 +0100

    making things actually work

--- pio_enconder/quadrature_encoder.c
+++ pio_enconder/quadrature_encoder.c
@@ -4,66 +4,130 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include "py/obj.h"
+#include "py/runtime.h"
 #include "hardware/pio.h"
-#include "hardware/timer.h"
-
 #include "quadrature_encoder.pio.h"
 
-//
-// ---- quadrature encoder interface example
-//
-// the PIO program reads phase A/B of a quadrature encoder and increments or
-// decrements an internal counter to keep the current absolute step count
-// updated. At any point, the main code can query the current count by using
-// the quadrature_encoder_*_count functions. The counter is kept in a full
-// 32 bit register that just wraps around. Two's complement arithmetic means
-// that it can be interpreted as a 32-bit signed or unsigned value, and it will
-// work anyway.
-//
-// As an example, a two wheel robot being controlled at 100Hz, can use two
-// state machines to read the two encoders and in the main control loop it can
-// simply ask for the current encoder counts to get the absolute step count. It
-// can also subtract the values from the last sample to check how many steps
-// each wheel as done since the last sample period.
-//
-// One advantage of this approach is that it requires zero CPU time to keep the
-// encoder count updated and because of that it supports very high step rates.
-//
+typedef struct _mp_obj_encoder_t {
+    mp_obj_base_t base;
+    PIO pio;
+    uint sm;
+    uint pin_ab;
+} mp_obj_encoder_t;
 
-int main() {
-    int new_value, delta, old_value = 0;
-    int last_value = -1, last_delta = -1;
+// MicroPython constructor: Encoder(pin_a)
+STATIC mp_obj_t encoder_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+    mp_arg_check_num(n_args, n_kw, 1, 1, false);
+    mp_obj_encoder_t *self = m_new_obj(mp_obj_encoder_t);
+    self->base.type = type;
+    self->pio = pio0;
+    self->sm = 0;
+    self->pin_ab = mp_obj_get_int(args[0]);
 
-    // Base pin to connect the A phase of the encoder.
-    // The B phase must be connected to the next pin
-    const uint PIN_AB = 10;
+    // Load and init PIO program
+    uint offset = pio_add_program(self->pio, &quadrature_encoder_program);
+    quadrature_encoder_program_init(self->pio, self->sm, self->pin_ab, offset);
 
-    stdio_init_all();
-    printf("Hello from quadrature encoder\n");
-
-    PIO pio = pio0;
-    const uint sm = 0;
-
-    // we don't really need to keep the offset, as this program must be loaded
-    // at offset 0
-    pio_add_program(pio, &quadrature_encoder_program);
-    quadrature_encoder_program_init(pio, sm, PIN_AB, 0);
-
-    while (1) {
-        // note: thanks to two's complement arithmetic delta will always
-        // be correct even when new_value wraps around MAXINT / MININT
-        new_value = quadrature_encoder_get_count(pio, sm);
-        delta = new_value - old_value;
-        old_value = new_value;
-
-        if (new_value != last_value || delta != last_delta ) {
-            printf("position %8d, delta %6d\n", new_value, delta);
-            last_value = new_value;
-            last_delta = delta;
-        }
-        sleep_ms(100);
-    }
+    return MP_OBJ_FROM_PTR(self);
 }
+
+// MicroPython method: read()
+STATIC mp_obj_t encoder_read(mp_obj_t self_in) {
+    mp_obj_encoder_t *self = MP_OBJ_TO_PTR(self_in);
+    int32_t count = quadrature_encoder_get_count(self->pio, self->sm);
+    return mp_obj_new_int(count);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(encoder_read_obj, encoder_read);
+
+// Method table
+STATIC const mp_rom_map_elem_t encoder_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_read), MP_ROM_PTR(&encoder_read_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(encoder_locals_dict, encoder_locals_dict_table);
+
+// Type definition
+STATIC const mp_obj_type_t encoder_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_Encoder,
+    .make_new = encoder_make_new,
+    .locals_dict = (mp_obj_dict_t*)&encoder_locals_dict,
+};
+
+// Module globals
+STATIC const mp_rom_map_elem_t encoder_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_encoder) },
+    { MP_ROM_QSTR(MP_QSTR_Encoder), MP_ROM_PTR(&encoder_type) },
+};
+STATIC MP_DEFINE_CONST_DICT(encoder_module_globals, encoder_module_globals_table);
+
+const mp_obj_module_t encoder_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&encoder_module_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_encoder, encoder_user_cmodule, MODULE_ENCODER_ENABLED);
+#include "py/obj.h"
+#include "py/runtime.h"
+#include "hardware/pio.h"
+#include "quadrature_encoder.pio.h"
+
+typedef struct _mp_obj_encoder_t {
+    mp_obj_base_t base;
+    PIO pio;
+    uint sm;
+    uint pin_ab;
+} mp_obj_encoder_t;
+
+// MicroPython constructor: Encoder(pin_a)
+STATIC mp_obj_t encoder_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+    mp_arg_check_num(n_args, n_kw, 1, 1, false);
+    mp_obj_encoder_t *self = m_new_obj(mp_obj_encoder_t);
+    self->base.type = type;
+    self->pio = pio0;
+    self->sm = 0;
+    self->pin_ab = mp_obj_get_int(args[0]);
+
+    // Load and init PIO program
+    uint offset = pio_add_program(self->pio, &quadrature_encoder_program);
+    quadrature_encoder_program_init(self->pio, self->sm, self->pin_ab, offset);
+
+    return MP_OBJ_FROM_PTR(self);
+}
+
+// MicroPython method: read()
+STATIC mp_obj_t encoder_read(mp_obj_t self_in) {
+    mp_obj_encoder_t *self = MP_OBJ_TO_PTR(self_in);
+    int32_t count = quadrature_encoder_get_count(self->pio, self->sm);
+    return mp_obj_new_int(count);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(encoder_read_obj, encoder_read);
+
+// Method table
+STATIC const mp_rom_map_elem_t encoder_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_read), MP_ROM_PTR(&encoder_read_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(encoder_locals_dict, encoder_locals_dict_table);
+
+// Type definition
+STATIC const mp_obj_type_t encoder_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_Encoder,
+    .make_new = encoder_make_new,
+    .locals_dict = (mp_obj_dict_t*)&encoder_locals_dict,
+};
+
+// Module globals
+STATIC const mp_rom_map_elem_t encoder_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_encoder) },
+    { MP_ROM_QSTR(MP_QSTR_Encoder), MP_ROM_PTR(&encoder_type) },
+};
+STATIC MP_DEFINE_CONST_DICT(encoder_module_globals, encoder_module_globals_table);
+
+const mp_obj_module_t encoder_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&encoder_module_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_encoder, encoder_user_cmodule, MODULE_ENCODER_ENABLED);
 
